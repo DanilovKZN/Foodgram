60e6ea0ef38daa33f45e738e39d80bb34d6893d2
Николай Данилов <danilov.nikola2018j@yandex.ru>
2022-06-30 22:28:08 +0300

    Foodgram ver.1.1.2

--- backend/api_foodgram/api_foodgram/settings.py
+++ backend/api_foodgram/api_foodgram/settings.py
@@ -104,8 +104,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/back_static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'back_static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'media')
