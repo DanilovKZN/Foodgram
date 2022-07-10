2ee770a24d99c4bc28584ab23821e08c7337301b
Николай Данилов <danilov.nikola2018j@yandex.ru>
2022-07-10 21:50:12 +0300

    1.3.6

--- backend/api_foodgram/api/serializers.py
+++ backend/api_foodgram/api/serializers.py
@@ -240,7 +240,7 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
             except Exception:
                 raise serializers.ValidationError(
                     {
-                        'ingredients': (VAL_NOT_INT)
+                        'ingredients': int(ingredient_item['amount'])
                     }
                 )
         if len(ingr_set) < len(ingredients):
